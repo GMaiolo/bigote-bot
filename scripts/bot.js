c9b7959b5248b5ae605cd51d4b8d976b5e989359
Nicolas Jeremías <nicolasje@lagash.com>
2017-07-23 20:36:43 -0300

    Welcome message in random channel

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
 const talkAboutNamedPeople = require('./people.talk')
 const { takeChance } = require('./utils')
+const definitions = require('./definitions')
 const client = new Discord.Client()
 
 client.on('ready', () => console.log('Bot ready.'));
 
 client.on('message', message => {
-    /* avoid execution if parsed message is from a bot */
+    /* avoid execution if parsed message is from a bot */    
     if (message.author.bot) return
     const gotLucky = takeChance(10)
     let answer
@@ -18,12 +19,13 @@ client.on('message', message => {
     }
 })
 
-client.on('guildMemberAdd', (guild, member) => {
-    console.log(`${member.user.username} joined ${guild.name}`)
-    const channel = client.channels.find('random', 'member-log')
-    
+client.on('guildMemberAdd', (member) => {
+    const channel = client.channels.find('name', definitions.channels.random)
+    const everyone = member.guild.roles.find('name', definitions.roles.everyone)
+    const greetingMessage = definitions.getGreetingMessage();
+
     if (!channel) return
-    channel.send(`Welcome to cartulo, bigote ${member}`)
+    channel.send(`${everyone}, ${greetingMessage} Welcome ${member}!`)
 })
 
 client.login(process.env.BIGOTE_BOT_TOKEN)
